b17108f705e55cc246a4e85f96e8fbff84158ae1
William.Weng <William.Weng@iit.com.hk>
2025-01-15 10:02:32 +0800

    - update package

--- Example/Example/ViewController/ViewController.swift
+++ Example/Example/ViewController/ViewController.swift
@@ -16,7 +16,7 @@ final class ViewController: UIViewController {
     @IBOutlet weak var myTextField: UITextField!
     @IBOutlet weak var myLabel: UILabel!
     
-    let connection = WWTcpConnection(host: "127.0.0.1", port: 8080, using: .tcp)
+    let connection = WWTcpConnection(host: "127.0.0.1", port: 8080)
     
     override func viewDidLoad() {
         super.viewDidLoad()
